f2635cd4602dc78bc11816e6cb8acff20757810e
amitadmin <amitadmin@192.168.0.103>
2018-02-12 00:10:08 +0530

    Conflict

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld
   public static void main (String args[])
   {
 	  System.out.println("Hi My name is Amit");
-	  System.out.println("Test Changes Again");
+	  System.out.println("Test Changes Again ");
   }
 }
