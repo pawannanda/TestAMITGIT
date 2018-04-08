70e4e1fd2c22fa221aee812b0d2f00e8395c5b6f
amitadmin <amitnanda0218@gmail.com>
2018-04-08 21:18:12 +0100

    Testing GIT commit / clone

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,9 +1,16 @@
 
 public class HelloWorld 
 {
-  public static void main (String args[])
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "HelloWorld{}";
+    }
+
+    public static void main (String args[])
   {
 	  System.out.println("Hi My name is Amit");
 	  System.out.println("Test Changes Again ");
+	  System.out.println("Test Changes Again ");
+
   }
 }
